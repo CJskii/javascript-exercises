ee9b6395eb2d0db5c9042c991136f9e552c0b685
CJskii <czareczarek3@gmail.com>
2022-07-08 21:26:07 +0100

    tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let newString = "";
+    for (i = str.length - 1; i >= 0; i--){
+        newString += str[i];
+    } 
+    console.log(newString);
+    return newString;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+reverseString("Hello");
+
