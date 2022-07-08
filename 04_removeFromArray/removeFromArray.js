e78455c92da5b036c2da9e1eb0a361e9d73cbfb8
CJskii <czareczarek3@gmail.com>
2022-07-08 23:38:49 +0100

    test 1 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,27 @@
-const removeFromArray = function() {
+/*  TEST 1
+const removeFromArray = function(arr, arg) {
+    let index = arr.indexOf(arg);
+    if (index > -1){
+        arr.splice(index, 1);
+        return arr;
+    }  
+}; */
 
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+const removeFromArray = function(arr, arg) {
+    let filtered = arr.filter
+    return arr.filtered;
+    
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+// create function that takes [arr] and ...args
+// function looks at [arr] and filters for given ...args
+// ...args goes into different variable
+// function returns [arr] without filtered values(...args) 
+
+//filter arguments [3, 2] from [1, 2, 3, 4] and return [1, 4]
+
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));  // should equal to [1, 4]
