49f0ca23400ed1cf8c28f0b214a1c594bdd45618
CJskii <czareczarek3@gmail.com>
2022-07-09 15:48:39 +0100

    tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,46 +1,23 @@
-/*  TEST 1
-const removeFromArray = function(arr, arg) {
-    let index = arr.indexOf(arg);
-    if (index > -1){
-        arr.splice(index, 1);
-        return arr;
-    }  
-}; */
-
-/*
-test('can remove all values', () => {
-    expect(removeFromArray([1, 2, 3, 4], 1, 2, 3, 4)).toEqual([]);
-  });
-
+const removeFromArray = function(arr, ...args){
+    let argsArray = Array.from(args);
+    
+    for (i = 0; i < arr.length; i++)
+        for(j=0; j < argsArray.length; j++)
+        if (arr[i] === argsArray[j]){
+            arr.splice(i, 1);
+            i--;
+            j--;
+        }
+        return arr
+};  
 
 
-*/
 
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-const removeFromArray = function(arr, arg, value) {
-    const filter = arr.filter((arr) => arr !== arg && arr !== value)
-    return filter;    
-};
-
-/*const removeFromArray = function(array, ...args) {
-
-    let args1 = Array.from(args);
-
-    for (i = 0; i < array.length; i++)
-        for (j = 0; j < args1.length; j++)
-
-    if (array[i] === args1[j]) {
-        array.splice(i, 1);
-        i--;
-        j--;
-    }
-    return array;
-}*/
+console.log(removeFromArray([1, 2, 3, 4, 5], 1, 3, 2))
 
 //console.log(removeFromArray);
 
 // Do not edit below this line
 module.exports = removeFromArray;
 
-console.log(removeFromArray([1, 2, 3, 4],1, 2, 3, 4));  // should equal to [1, 4]
+//console.log(removeFromArray([1, 2, 3, 4],1, 2, 3, 4));  // should equal to [1, 4]
