e2fae2438844603132465203eb2cf1ea5cab828b
CJskii <czareczarek3@gmail.com>
2022-07-10 15:52:46 +0100

    tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
+let tempF = 32
+let tempC = 0
 
+const ftoc = function(tempF) {
+  let celcius = (5/9) * (tempF - 32);
+  celcius = celcius.toFixed(1);
+  return Number(celcius);
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  let fahrenheit = 32 + (tempC * 1.8);
+  fahrenheit = fahrenheit.toFixed(1);
+  return Number(fahrenheit);
 };
 
 // Do not edit below this line
