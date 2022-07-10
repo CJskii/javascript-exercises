515ec0ff85c6c2493573d2567a016d7d2a73cb27
CJskii <czareczarek3@gmail.com>
2022-07-10 16:58:17 +0100

    tests passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,44 @@
-const add = function() {
-	
+const add = function(a, b) {
+  let value = a + b;
+  return Number(value);	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  let value = a - b;
+  return Number(value);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let value = 0;
+  for (i = 0; i < arr.length; i++){
+    value += arr[i];
+  } return value;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let value = 1
+  for (i = 0; i < arr.length; i++){
+    value *= arr[i];
+  } return Number(value);
+};
+ 
 
+const power = function(a, b) {
+	let value = Math.pow(a, b);
+  return value;
 };
 
-const power = function() {
-	
+const factorial = function(num) {
+	let array = []
+  let value = 1
+  for (i = 1; i <= num; i++)
+    array.unshift(i)
+  for (j = 0; j < array.length; j++){
+    value *= array[j];
+  } return Number(value);
 };
 
-const factorial = function() {
-	
-};
+console.log(factorial(5));
 
 // Do not edit below this line
 module.exports = {
@@ -31,3 +49,8 @@ module.exports = {
   power,
   factorial
 };
+/*
+arr = [5, 4, 3, 2, 1]
+let i = 6
+arr.unshift(i);
+console.log(arr); */
