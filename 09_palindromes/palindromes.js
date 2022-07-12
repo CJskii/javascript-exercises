00225197c3a4773f3ff816614eecbe441a01703e
CJskii <czareczarek3@gmail.com>
2022-07-12 01:20:00 +0100

    2 tests passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let re = /[^A-Za-z0-9]/g; 
+    let length = str.length;
+    str = str.toLowerCase().replace(re, '')
+    for (i = 0; i <= length/2; i++)
+    if (str[i] !== str[length - 1 - i]){
+        return false;
+    }
+    return true;
 };
+
+console.log(palindromes("sTRINg"));
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+//palindromes('racecar') // true
+//palindromes('tacos') // false
