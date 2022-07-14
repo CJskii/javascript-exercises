4823ea5676ad23e03185b371226e2cd8a8d42642
CJskii <czareczarek3@gmail.com>
2022-07-14 23:37:03 +0100

    tests passed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    let array 
+    array = books.map((item) => {
+        return item.title
+    }, 1);
+        return array;
+    };
 
-};
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
