56ac358991c801106700f9e0cad54848463186ce
CJskii <czareczarek3@gmail.com>
2022-07-15 02:17:17 +0100

    test 1 passed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,39 @@
-const findTheOldest = function() {
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
 
-};
+const getAge = ({ yearOfBirth, yearOfDeath }) => yearOfDeath - yearOfBirth; // use 2 objects to calculate age
+const findTheOldest = function(people) {
+  let age = getAge(people[0]); 
+  return people.reduce((group, person) => {
+    const personAge = getAge(person);
+    if (personAge > age) {
+      age = personAge;
+      return person;
+    }
+    return group;
+  });
+}
+
+
+
+console.log(findTheOldest(people));
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
